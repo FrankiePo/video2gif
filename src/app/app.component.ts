d3c2b363e1252550f6269bec4ea2a3fe7019f608
Sergey Koshechkin <serge.koshechkin@protonmail.com>
2018-04-02 02:55:43 +0300

    add simple drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  dragover = false;
+  loading = false;
+  image: HTMLImageElement;
+  form = new FormGroup({});
+  data = {
+    image: null,
+  };
+
+  handlePhotoInputChange(e: Event) {
+    const target: HTMLInputElement = (e.target || e.srcElement) as HTMLInputElement;
+    if (target && target.files && target.files.length > 0) {
+      const file = target.files[0];
+      this.setFile(file);
+    }
+  }
+
+  setFile(file: File) {
+    console.log(file);
+    const image: HTMLImageElement = new Image();
+    const myReader: FileReader = new FileReader();
+    myReader.onloadend = (loadEvent: any) => {
+      image.src = loadEvent.target.result;
+    };
+
+    myReader.readAsDataURL(file);
+  }
+
+  preventEvent(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  dragOver(event: DragEvent) {
+    this.dragover = true;
+    this.preventEvent(event);
+  }
+  dragEnd(event: DragEvent) {
+    this.dragover = false;
+    this.preventEvent(event);
+  }
+  drop(event: DragEvent) {
+    const file = event.dataTransfer.files[0];
+    this.setFile(file);
+    this.dragover = false;
+    this.preventEvent(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
